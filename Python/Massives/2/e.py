f11d1d4b3485f092ab345f95bcbbd24a23033399
Sk7Str1pe <159684143+Sk7Str1pe@users.noreply.github.com>
2024-10-30 16:11:47 +0300

    Add files via upload

---
+++ Python/Massives/2/e.py
@@ -0,0 +1,24 @@
+n, m = map(int, input().split())
+A = []
+C = []
+for i in range(n): # формирование матрицы A
+	b = list(map(int, input().split())) # считывается по-строчно с формированием списка
+	A.append(b)
+
+max_zn = 0
+max_sum = 0
+max_i = 0
+max_j = 0
+for i in range(n):
+	if (max(A[i]) > max_zn):
+		max_zn = max(A[i])
+
+for i in range(n):
+	if (max_zn in A[i]):
+		C.append(i)	# массив строк, где есть максимальное значение
+
+for i in range(len(C)):
+	if (sum(A[C[i]]) > max_sum):
+		max_i = C[i]
+		max_sum = sum(A[C[i]])
+print(max_i)
